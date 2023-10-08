ff9fee4df7e9bd78c46db1c86e172b3a60679f41
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2023-10-09 00:46:35 +0530

    Construction Site Backend Complete

--- Backend/routes/manager-router.js
+++ Backend/routes/manager-router.js
@@ -1,4 +1,4 @@
-import { addManager, getAllManagers, getManagerByUserId, getManagerByManagerId, deleteManager, updateManagerByManagerId} from '../controllers/manager-controller.js';
+import { addManager, getAllManagers, getManagerByUserId, getManagerByManagerId, deleteManager, updateManagerByManagerId,getAllManagerIds} from '../controllers/manager-controller.js';
 import express from 'express';
 
 const manager_router = express.Router();
@@ -7,6 +7,7 @@ manager_router.post("/addManager", addManager);
 manager_router.get("/", getAllManagers);
 manager_router.get("/managerbyId/:managerId", getManagerByManagerId);
 manager_router.get('/managerbyuserId/:userId', getManagerByUserId);
+manager_router.get('/getallmanagerIds', getAllManagerIds);
 manager_router.patch("/updatemanager/:managerId", updateManagerByManagerId);
 manager_router.delete("/deleteManager/:id", deleteManager);
 
