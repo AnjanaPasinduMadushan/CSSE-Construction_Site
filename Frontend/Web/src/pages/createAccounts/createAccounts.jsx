316777cbd46d1209971104fadbba75b882cd2556
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-10-16 20:11:16 +0530

    final source code

---
+++ Frontend/Web/src/pages/createAccounts/createAccounts.jsx
@@ -0,0 +1,12 @@
+import CreateAcc from "../../components/createAccForm"
+
+const CreateAccounts = () => {
+
+  return (
+    <>
+      <CreateAcc />
+    </>
+  )
+}
+
+export default CreateAccounts
