c11b192dfdd8f4a67a5bb5233e496fe2f8288c22
IT21021534-Priyantha <it21021534@my.sliit.lk>
2023-10-23 23:28:50 +0530

    minor optimisations

--- Backend/controllers/constructionsite-controller.js
+++ Backend/controllers/constructionsite-controller.js
@@ -1,6 +1,7 @@
 import ConstructionSiteDTO from "../DTO/construction-siteDTO.js";
 import ConstructionSite from "../models/construction-site/constructionsite-model.js";
 
+// Create
 const addConstructionSite = async (req, res) => {
     const { ConstructionSiteDTO } = req.body;
 
@@ -51,8 +52,17 @@ const addConstructionSite = async (req, res) => {
 const updateSiterBySiteId = async (req, res) => {
     try {
         const { siteId } = req.params;
-        const { siteName, location, address, province, description, startDate, endDate, managerID } = req.body;
-        console.log(siteId)
+        const {
+            siteName,
+            location,
+            address,
+            province,
+            description,
+            startDate,
+            endDate,
+            managerID,
+        } = req.body;
+
         // Find the manager by managerId
         const existingSite = await ConstructionSite.findOne({ siteID: siteId });
 
@@ -141,64 +151,67 @@ const getAllSites = async (req, res) => {
     }
 };
 
-// Get all site by managerId
 const getSitesByManagerId = async (req, res) => {
     try {
         const { managerId } = req.params;
 
-        // Find the site by siteId
+        // Use findOne to find a single site with the given managerID
         const site = await ConstructionSite.find({ managerID: managerId });
 
         if (!site) {
+            // Return a 404 response if the site is not found
             return res.status(404).json({ message: "Site not found" });
         }
 
+        // Return a 200 response with the site data
         return res.status(200).json(site);
     } catch (err) {
         console.error(err);
-        return res.status(400).json({ message: "Error occurred in fetching Site" });
+        // Return a 500 response for any unexpected errors
+        return res.status(500).json({ message: "Error occurred in fetching Site" });
     }
 };
 
+
 const getSiteCountsByProvince = async () => {
     try {
-      // Find all sites
-      const sites = await ConstructionSite.find();
-  
-      if (!sites || sites.length === 0) {
-        return { error: "No sites found" };
-      }
-  
-      // Group sites by province and year of endDate
-      const siteCountsByProvinceAndYear = {};
-      sites.forEach((site) => {
-        const endDateParts = site.endDate.split('/');
-        if (endDateParts.length === 3) {
-          const year = endDateParts[2]; // Extract the year from endDate
-          const key = `${site.province}_${year}`;
-          if (siteCountsByProvinceAndYear[key]) {
-            siteCountsByProvinceAndYear[key]++;
-          } else {
-            siteCountsByProvinceAndYear[key] = 1;
-          }
+        // Find all sites
+        const sites = await ConstructionSite.find();
+
+        if (!sites || sites.length === 0) {
+            return { error: "No sites found" };
         }
-      });
-  
-      // Convert the grouped data into an array of objects with province and count
-      const resultArray = Object.entries(siteCountsByProvinceAndYear).map(([key, count]) => {
-        const [province, year] = key.split('_');
-        return {
-          province,
-          year,
-          count,
-        };
-      });
-  
-      return resultArray;
+
+        // Group sites by province and year of endDate
+        const siteCountsByProvinceAndYear = {};
+        sites.forEach((site) => {
+            const endDateParts = site.endDate.split('/');
+            if (endDateParts.length === 3) {
+                const year = endDateParts[2]; // Extract the year from endDate
+                const key = `${site.province}_${year}`;
+                if (siteCountsByProvinceAndYear[key]) {
+                    siteCountsByProvinceAndYear[key]++;
+                } else {
+                    siteCountsByProvinceAndYear[key] = 1;
+                }
+            }
+        });
+
+        // Convert the grouped data into an array of objects with province and count
+        const resultArray = Object.entries(siteCountsByProvinceAndYear).map(([key, count]) => {
+            const [province, year] = key.split('_');
+            return {
+                province,
+                year,
+                count,
+            };
+        });
+
+        return resultArray;
     } catch (err) {
-      console.error(err);
-      return { error: "Error occurred in fetching Sites" };
+        console.error(err);
+        return { error: "Error occurred in fetching Sites" };
     }
-  };
+};
 
 export { addConstructionSite, updateSiterBySiteId, deleteConstructionSite, getSiteBySiteId, getAllSites, getSitesByManagerId, getSiteCountsByProvince };
